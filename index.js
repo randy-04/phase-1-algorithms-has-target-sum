88a16a07ab7a64145ebc4aae541cfdc353d17ec3
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-12-08 11:28:47 +0300

    First trial

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // look through array from the first digit
+  for (let i = 0; i < array.length; i++) {
+    // look through the rest of the array
+    for (let j=i+1; j <array.length; j++) {
+      // adding i and j
+      const sum = array[i] + array[j]
+      if (sum === target) {
+        return true;
+      }
+      
+    }
+  }
 }
 
 /* 
